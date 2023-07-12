ca63f232748f8e291b9bc1a26454c5e4b482cdc1
shakespear95 <59427923+shakespear95@users.noreply.github.com>
2023-07-12 23:22:38 +0800

    fixed some of the sections

--- index.js
+++ index.js
@@ -19,3 +19,29 @@ function rotateProfession() {
 }
 
 setInterval(rotateProfession, 2000); // Adjust the interval time (in milliseconds) here
+
+const imageContainers = document.querySelectorAll('.image-container');
+const prevButton = document.querySelector('.prev-button');
+const nextButton = document.querySelector('.next-button');
+let currentIndex = 0;
+
+function showImage(index) {
+  imageContainers.forEach((container, i) => {
+    container.style.display = (i === index) ? 'block' : 'none';
+  });
+}
+
+prevButton.addEventListener('click', () => {
+  currentIndex = (currentIndex === 0) ? imageContainers.length - 1 : currentIndex - 1;
+  showImage(currentIndex);
+});
+
+nextButton.addEventListener('click', () => {
+  currentIndex = (currentIndex === imageContainers.length - 1) ? 0 : currentIndex + 1;
+  showImage(currentIndex);
+});
+
+
+
+
+
